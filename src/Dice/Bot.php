b12beb9c779282bf9b0594fdc3989f9a8bf59863
Ida Lam <idaslam@outlook.com>
2020-04-17 19:06:51 +0200

    Bot controller for Dice game

--- src/Dice/Bot.php
+++ src/Dice/Bot.php
@@ -1,10 +1,32 @@
 <?php
-namespace Ida\Guess;
 
-/**
- * Guess my number, a class supporting the game through GET, POST and SESSION.
- */
-class Bot
+namespace Ida\Dice;
+
+class Bot extends Player
 {
-    
+    public $rollCount = 0;
+    private $maxRoll = 4;
+
+    public function __construct($diceCount)
+    {
+        parent::__construct($diceCount);
+        $this->rollCount = rand(1, $this->maxRoll);
+    }
+
+    public function BotRoll()
+    {
+        if ($this->rollCount > 0) {
+            parent::roll();
+            $this->rollCount -= 1;
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public function newRollCount()
+    {
+        $this->rollCount = rand(1, $this->maxRoll);
+    }
 }
