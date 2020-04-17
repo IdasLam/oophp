fd1a12557c1b58630d7c0dab9e9436d6f7f4ad2c
Ida Lam <idaslam@outlook.com>
2020-04-17 19:07:04 +0200

    Main controller of Dice game

--- src/Dice/DiceGame.php
+++ src/Dice/DiceGame.php
@@ -8,162 +8,99 @@ class DiceGame
 {
     /**
      * @var int $players   Current players.
-     * @var int $dice      Dices.
-     * @var int $round     rounds.
-     * @var int $turn     Turn.
-     * @var int $playerOrder     Order of players.
-     * @var int $currentTurn     Whos turn.
-     * @var int $points    Players points.
      */
-
-    private $players = null;
-    private $dice = [];
-    private $round = 0;
-    private $turn = [];
+    private $players = [];
     private $playerOrder = [];
-    private $currentTurn = null;
-    private $points = [];
+    private $playerTurn = null;
+    private $finishedGame = false;
 
-    /**
-     * Set players count
-     * @param int $count The player count. Default 2.
-     * @param int $dice  Dices. Default 1.
-     */
-    public function setGame(int $count = 2, int $dice = 1)
+    public function __construct(int $playerCount, int $diceCount)
     {
-        $this->players = $count;
+        for ($i = 0; $i < $playerCount; $i++) {
 
-        for ($i = 1; $i <= $dice; $i++) {
-            $this->dice[] = new Dice();
+            $this->players[$i] = $i > 0 ? new Bot($diceCount) : new Player($diceCount);
         }
     }
 
-    /**
-     * Set players turn
-     * @param int $player The player.
-     */
-    public function setTurn(int $player)
-    {   
-        $playerName = "$player";
-
-        if (empty($this->turn)) {
-            $this->currentTurn = $playerName;
-        }
-
-        array_push($this->turn, $playerName);
-    }
-
-    /**
-     * Roll dice for a player
-     * @param player
-     */
-    public function rollDice($player, $start = false) {
-        $rolls = [];
-        $dices = count($this->dice);
-        
-        for ($i = 0; $i < $dices; $i++) {
-            $this->dice[$i]->roll();
-            $face = $this->dice[$i]->getFace();
-            $rolls[] = $face;
-        }
-        
-        if ($start === true) {
-            $sum = 0;
-            foreach ($rolls as $face) {
-                $sum += $face;
-            }
-
-            $this->playerOrder["p$player"] = $sum;
-
-            $this->turn["p$player"] = $rolls;
-        } else {
-            $this->turn = [];
-
-            $this->turn[$player] = $rolls;
-        }
-    }
-    
-    
-    
-    /**
-     * Get playerOrder, and sort by highest key
-     * @return int as playerOrder 
-     */
-    public function getplayerOrder()
-    {
-        arsort($this->playerOrder);
-        return $this->playerOrder;
-    }
-
-    /**
-     * Get turn, and sort by highest key
-     * @return int as turn 
-     */
-    public function getTurn()
-    {
-        return $this->turn;
-    }
-
-    /**
-     * Set current turn
-     * @param player the player turn
-     */
-    public function setOrder($player)
-    {
-        $this->currentTurn = $player;
-    }
-
-    /**
-     * Set current turn
-     * @param player the player turn
-     */
-    public function setCurrentTurn($player)
-    {
-        $this->currentTurn = $player;
-    }
-
-    /**
-     * Get current turn
-     * @return player
-     */
-    public function getCurrentTurn()
-    {
-        return $this->currentTurn;
-    }
-
-    /**
-     * Get players count
-     * @return int as players count 
-     */
     public function getPlayers()
     {
         return $this->players;
     }
-    
-    /**
-     * Get dice count
-     * @return int as dice count 
-     */
-    public function getDice()
+
+    public function hasOrder()
     {
-        return $this->dice;
-    }
-    
-    /**
-     * Get round
-     * @return int as round 
-     */
-    public function getRound()
-    {
-        return $this->round;
+        return !empty($this->playerOrder);
     }
 
-    /**
-     * Set round
-     */
-    public function nextRound()
-    {
-        $this->round += 1;
+    public function playerRoll() {
+        $this->players[$this->playerTurn]->roll();
+        $rolledOne = $this->players[$this->playerTurn]->hasRolledOne();
+
+        if ($rolledOne) {
+            $this->players[$this->playerTurn]->resetRoundScore();
+            $this->players[$this->playerTurn]->endTurn();
+            $this->nextTurn();
+        }
     }
 
+    public function BotRoll() {
+        $isDone = $this->players[$this->playerTurn]->BotRoll();
+        $rolledOne = $this->players[$this->playerTurn]->hasRolledOne();
+
+        if ($rolledOne || $isDone) {
+            $this->players[$this->playerTurn]->endTurn();
+            $this->players[$this->playerTurn]->newRollCount();
+            $this->nextTurn();
+        }
+    }
+
+    public function nextTurn() {
+        $this->players[$this->playerTurn]->endTurn();
+        $playerScore = $this->players[$this->playerTurn]->getScore();
+
+        if ($playerScore >= 100) {
+            $this->finishedGame = true;
+        } else {
+            $index = array_search($this->playerTurn, $this->playerOrder);
+    
+            if ($index === count($this->playerOrder) - 1) {
+                $this->playerTurn = $this->playerOrder[0];
+            } else {
+                $this->playerTurn = $this->playerOrder[$index + 1];
+            }
+        }
+    }
+
+    public function setOrder()
+    {
+        $order = [];
+
+        for ($i = 0; $i < count($this->players); $i++) {
+            $this->players[$i]->roll();
+            $this->players[$i]->resetScore();
+            $score = array_sum($this->players[$i]->getRoll());
+            
+            $order[$i] = $score;
+        }
+
+        arsort($order);
+
+        foreach($order as $key => $value) {
+            $this->playerOrder[] = $key;
+        }
+
+        $this->playerTurn = $this->playerOrder[0];
+    }
+
+    public function getOrder() {
+        return $this->playerOrder;
+    }
+
+    public function getPlayerTurn() {
+        return $this->playerTurn;
+    }
+
+    public function getfinishedGame() {
+        return $this->finishedGame;
+    }
 }
